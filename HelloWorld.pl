cd4491280d0e15cdf23e818a4cb50138a97055de
seaskyv <haipengzhang28@gmail.com>
2021-07-07 19:57:26 +1000

    Update to main branch.

--- HelloWorld.pl
+++ HelloWorld.pl
@@ -4,4 +4,4 @@ print("Hello World");
 print("Cloned to VS");
 print("tried to start a new branch");
 print("Create a new branch from pervious by VS");
-print("update on file to main branch");
+print("update on file to main branch.");
